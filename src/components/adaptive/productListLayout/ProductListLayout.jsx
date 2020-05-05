7de4d94f70b6f8dc6bb238f463750d62f65d835d
dimitrisafendras <dimitris.afendras@gmail.com>
2020-05-05 12:28:01 +0300

    finished

--- src/components/adaptive/productListLayout/ProductListLayout.jsx
+++ src/components/adaptive/productListLayout/ProductListLayout.jsx
@@ -6,9 +6,13 @@ export const ProductListLayout = ({}) => {
 		<div className='ProductListLayout'>
 			<div className='Breadcrumb'></div>
 			<div className='Title'></div>
-			<div className='Filters'></div>
+			<div className='Filters'>
+				<div className='Filter'></div>
+			</div>
 			<div className='MoreFilters'></div>
-			<div className='Products'></div>
+			<div className='Products'>
+				<div className='Product'></div>
+			</div>
 		</div>
 	);
 };
