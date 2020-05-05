58de6c6ca45c9f3e6e7bbe759a9838665fca9c9a
dimtirisafendras <dimitris.afendras@gmail.com>
2020-05-05 15:56:18 +0300

    fix

--- src/components/adaptive/categoryNavLayout/CategoryNavLayout.jsx
+++ src/components/adaptive/categoryNavLayout/CategoryNavLayout.jsx
@@ -4,15 +4,22 @@ import './CategoryNavLayout.css';
 export const CategoryNavLayout = ({}) => {
 	return (
 		<div className='CategoryNavLayout'>
-			<div className='Bg'></div>
-				<div className='ItemsContainer'>
-					<div className='Item'></div>
-					<div className='Item'></div>
-					<div className='Item'></div>
-					<div className='Item'></div>
-					<div className='Item'></div>
-					<div className='Item'></div>
-				</div>
+			<div className='Bg'>
+				<div className='MiddleBox'></div>
+			</div>
+			<div className='Items'>
+				<div className='Item'></div>
+				<div className='Item'></div>
+				<div className='Item'></div>
+				<div className='Item'></div>
+				<div className='Item'></div>
+				<div className='Item'></div>					<div className='Item'></div>
+				<div className='Item'></div>
+				<div className='Item'></div>
+				<div className='Item'></div>
+				<div className='Item'></div>
+				<div className='Item'></div>
+			</div>
 		</div>
 	);
 };
