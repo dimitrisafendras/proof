ffeb92ae129dc5a98f64c5272450a548b41c2ae3
dimtirisafendras <dimitris.afendras@gmail.com>
2020-05-05 10:55:09 +0300

    refactor

--- src/components/adaptive/productListLayout/ProductListLayout.jsx
+++ src/components/adaptive/productListLayout/ProductListLayout.jsx
@@ -4,6 +4,12 @@ import './ProductListLayout.css';
 export const ProductListLayout = ({}) => {
 	return (
 		<div className='ProductListLayout'>
+			<div className='Breadcrumb'></div>
+			<div className='Title'></div>
+			<div className='Filters'>
+				<div style={{height: '1000px'}}></div>
+			</div>
+			<div className='Products'></div>
 		</div>
 	);
 };
